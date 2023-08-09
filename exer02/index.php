10dfa0539e3c228cea437fda2e6801ccc4f7dd2b
Matheus Marques <marques.matheusferreira@gmail.com>
2023-08-08 21:19:14 -0300

    adding lesson 2

---
+++ exer02/index.php
@@ -0,0 +1,39 @@
+<?php
+// Include the database connection file
+require_once("dbConnection.php");
+
+// Fetch data in descending order (lastest entry first)
+$result = mysqli_query($mysqli, "SELECT * FROM users ORDER BY id DESC");
+?>
+
+<html>
+<head>	
+	<title>Home</title>
+</head>
+
+<body>
+	<h2>Home</h2>
+	<p>
+		<a href="add.php">Adicionar Dado</a>
+	</p>
+	<table width='80%' border=0>
+		<tr bgcolor='#DDDDDD'>
+			<td><strong>Nome</strong></td>
+			<td><strong>Idade</strong></td>
+			<td><strong>E-mail</strong></td>
+			<td><strong>Action</strong></td>
+		</tr>
+		<?php
+		// Fetch the next row of a result set as an associative array
+		while ($res = mysqli_fetch_assoc($result)) {
+			echo "<tr>";
+			echo "<td>".$res['name']."</td>";
+			echo "<td>".$res['age']."</td>";
+			echo "<td>".$res['email']."</td>";	
+			echo "<td><a href=\"edit.php?id=$res[id]\">Edit</a> | 
+			<a href=\"delete.php?id=$res[id]\" onClick=\"return confirm('Tem certeza que quer apagar?')\">Delete</a></td>";
+		}
+		?>
+	</table>
+</body>
+</html>
