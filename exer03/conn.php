eaf0c4e4a8eefee05e9284110a173b909ca4849e
Matheus Marques <marques.matheusferreira@gmail.com>
2023-08-25 19:17:37 -0300

    Adding exer03 folder

---
+++ exer03/conn.php
@@ -0,0 +1,8 @@
+<?php
+$databaseHost = '127.0.0.1';
+$databaseName = 'aula';
+$databaseUsername = 'root';
+$databasePassword = 't3dnpoa4';
+
+// Open a new connection to the MySQL server
+$mysqli = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName); 
