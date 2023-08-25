1201d254928338517506529101c210f31812d370
Matheus Marques <marques.matheusferreira@gmail.com>
2023-08-25 19:59:33 -0300

    adjusting files

--- exer03/add.php
+++ exer03/add.php
@@ -1,6 +1,6 @@
 <html>
 <head>
-	<title>Add Data</title>
+	<title>Adicionar Dados</title>
 </head>
 
 <body>
--- exer03/index.php
+++ exer03/index.php
@@ -27,8 +27,8 @@ $result = mysqli_query($mysqli, "SELECT * FROM users ORDER BY id DESC");
 		// Fetch the next row of a result set as an associative array
 		while ($res = mysqli_fetch_assoc($result)) {
 			echo "<tr>";
-			echo "<td>".$res['name']."</td>";
-			echo "<td>".$res['age']."</td>";
+			echo "<td>".$res['nome']."</td>";
+			echo "<td>".$res['idade']."</td>";
 			echo "<td>".$res['email']."</td>";	
 			echo "<td><a href=\"edit.php?id=$res[id]\">Edit</a> | 
 			<a href=\"delete.php?id=$res[id]\" onClick=\"return confirm('Tem certeza que quer apagar?')\">Delete</a></td>";
